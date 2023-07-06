762386262600e2b7653115e61323eb2e2381d7cc
SeasonPilot <pilot1@foxmail.com>
2023-07-06 17:33:18 +0800

    feat: 增加从命令行读取配置文件路径，并从配置文件data和dateStr参数

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 // 引入fs模块，用于操作文件系统
-const fs = require("fs");
+const fs = require('fs');
+// 引入process模块，用于获取命令行参数
+const process = require('process');
+// 引入path模块，用于处理文件路径
+const path = require('path');
+
+// 使用process.argv数组获取命令行参数，例如node jsonScript.js config.json
+const configFile = process.argv[2] || 'config.json';
+// 使用path.resolve函数将相对路径转换为绝对路径，例如path.resolve(__dirname, process.argv[2])
+const configPath = path.resolve(__dirname, configFile);
+
+// 使用require函数加载配置文件，例如const config = require(path.resolve(__dirname, process.argv[2]))
+const config = require(configPath);
 
 // 引入conversations.json文件，这是您导出的json数据
-const data = require("./conversations.json");
+const data = require(config.data);
+
+// 使用config.data和config.dateStr替换原来的硬编码参数，例如saveFile(config.data, config.dateStr)
+saveFile(data, config.dateStr);
 
 // 定义一个异步函数，用于将导出数据转换成markdown并保存为文件
 async function saveFile(exportData, dateStr) {
@@ -17,21 +32,19 @@ async function saveFile(exportData, dateStr) {
         if (sessionDate >= date) {
 
             // 定义一个空字符串，用于存储转换后的markdown内容
-            let output = "";
-
+            let output = '';
             if (obj.question == null || obj.conversationRecords.length === 0) {
-                return
+                return;
             }
 
             const lines = obj.question.split('\n');
             let text = lines[lines.length - 1];
             if (text.length > 100) {
-                text = text.substring(0, 100) // 从第 0 个字符开始截取到第100个字符
+                text = text.substring(0, 100); // 从第 0 个字符开始截取到第100个字符
             }
 
             // 将斜杠替换为空
-            text = text.replace(/\//g, "");
-
+            text = text.replace(/\//g, '');
             // 第一层循环遍历每个对象
             // 遍历导出数据中的conversationRecords数组，提取每一条对话的问题和答案
             obj.conversationRecords.forEach((data) => {
@@ -39,34 +52,34 @@ async function saveFile(exportData, dateStr) {
                 // 将问题和答案按照markdown的格式拼接到output字符串中，并加上分隔符
                 output += `Question:\n\n${data.question}\n\nAnswer:\n\n${data.answer}\n\n<hr/>\n\n`;
             });
-            writeFile(obj.sessionName, text, output)
+            writeFile(obj.sessionName, text, output);
         }
     });
 }
 
 function writeFile(sessionName, question, output) {
     // 将斜杠替换为短横线
-    const hyphenString = sessionName.replace(/\//g, "-");
-
+    const hyphenString = sessionName.replace(/\//g, '-');
     // 将空格替换为中划线
-    const outputString = hyphenString.replace(" ", "-");
-
-    const fileName = "./chatgptbox/" + outputString + question + ".md";
-
-    // todo: 判断文件是否已经存在，如果已经存在则跳过
-    fs.writeFile(fileName, output, (err) => {
-        // 如果发生错误，打印错误信息
+    const outputString = hyphenString.replace(' ', '-');
+    const fileName = `./chatgptbox/${outputString}${question}.md`;
+    // 判断文件是否已经存在，如果已经存在则跳过
+    fs.access(fileName, fs.constants.F_OK, (err) => {
         if (err) {
-            console.error(`fs.writeFile ERR:  ${err}`);
-            console.log(`Session name:  ${sessionName}`);
+            // 如果文件不存在，就调用fs.writeFile函数写入文件
+            fs.writeFile(fileName, output, (err) => {
+                // 如果发生错误，打印错误信息
+                if (err) {
+                    console.error(`fs.writeFile ERR: ${err}`);
+                    console.log(`Session name: ${sessionName}`);
+                } else {
+                    // 如果成功，打印成功信息
+                    console.log(`File saved as ${fileName}`);
+                }
+            });
         } else {
-            // 如果成功，打印成功信息
-            console.log(`File saved as ${fileName}`);
+            // 如果文件已经存在，就打印提示信息
+            console.log(`File already exists: ${fileName}`);
         }
     });
 }
-
-// 调用异步函数，并传入data变量和一个日期字符串作为参数，仅保存创建时间在日期之后的文章
-// noinspection JSIgnoredPromiseFromCall
-// use a comment to suppress the inspection
-saveFile(data, '2023/5/1 22:47:11');
